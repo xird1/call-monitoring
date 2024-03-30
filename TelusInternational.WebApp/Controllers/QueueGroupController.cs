a631a9cd234d5b6fa4d076382a8fa16fe010a7e6
xandrix <xandrix.l.enrico@dxc.com>
2024-03-30 12:10:05 +0800

    add interval sample data for demo for Audi

--- TelusInternational.WebApp/Controllers/QueueGroupController.cs
+++ TelusInternational.WebApp/Controllers/QueueGroupController.cs
@@ -21,6 +21,14 @@ namespace TelusInternational.WebApp.Controllers
         public async Task<IActionResult> Get()
         {
             var queueGroups = await _queueGroupService.GetAll();
+
+            // uncomment this to demonstrate refresh interval every 10 sec visually, assuming data is saved in database and then changes
+            // because right now data is just stored in a json file in memory and modifying records requires app relaunch to be read by context
+            queueGroups.Where(q => q.Name == "Audi").ToList().ForEach(f => {
+                 f.MonitorData.TalkTime = new Random().Next(1, 500);
+                 f.MonitorData.Offered = new Random().Next(1, 40);
+             });
+
             return Ok(queueGroups);
         }
     }
